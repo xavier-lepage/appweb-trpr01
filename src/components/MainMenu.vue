f7d15b4ea0e0d5266f805d22ab0eee50d41467b8
xavier-lepage <2445275@csfoy.ca>
2026-02-27 08:20:46 -0500

    Added product form to main menu
    Added ability to toggle the "Add product" form.

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,29 +1,58 @@
 <script setup lang="ts">
+import { Collapse } from 'bootstrap';
+import { ref, onMounted, useTemplateRef } from 'vue';
 import { Priority } from '../models/Priority';
+import { ProductAction } from '../models/ProductAction';
 import ActionButton from './ActionButton.vue';
 import Header from './Header.vue';
 import ProductForm from './ProductForm/ProductForm.vue';
+
+/*	Documentation utilisée pour le collapse bootstrap:
+	https://vuejs.org/guide/essentials/template-refs
+	https://vuejs.org/api/composition-api-lifecycle.html#onmounted
+	https://getbootstrap.com/docs/5.3/components/collapse/#via-javascript
+*/
+
+const currentAction = ref<ProductAction>(ProductAction.NONE);
+const formTitle = ref<string>('Ajouter un composant');
+const productForm = useTemplateRef("productForm");
+let collapse: Collapse;
+
+onMounted(() => {
+	if (productForm.value !== null) {
+		collapse = new Collapse(productForm.value, { toggle: false });
+	}
+});
+
+function addProduct(): void {
+	manageProductFormVisibility(ProductAction.ADD)
+}
+
+function manageProductFormVisibility(newAction: ProductAction): void {
+	if (currentAction.value === newAction) {
+		collapse.hide();
+		currentAction.value = ProductAction.NONE
+	} else {
+		formTitle.value = newAction
+		collapse.show();
+		currentAction.value = newAction
+	}
+}
 </script>
 
 <template>
 	<div class="container">
 		<Header></Header>
 
-		<ActionButton 
-			data-bs-toggle="collapse" 
-			data-bs-target="#productForm" 
-			aria-expanded="false" 
-			aria-controls="productForm" 
+		<ActionButton
+			class="mx-2 my-4"
+			@click="addProduct()" 
 			:label="'Ajouter un composant'" 
 			:priority="Priority.SECONDARY">
 		</ActionButton>
 
-		<div class="collapse" id="productForm">
-			<ProductForm></ProductForm>
+		<div class="collapse" ref="productForm">
+			<ProductForm :formTitle="formTitle"></ProductForm>
 		</div>
 	</div>
 </template>
-
-<style>
-
-</style>
--- src/components/ProductForm/ProductForm.vue
+++ src/components/ProductForm/ProductForm.vue
@@ -1,10 +1,57 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { productCategories, type ProductCategory } from '../../models/ProductCategory';
+let props = defineProps<{
+	formTitle: string
+}>();
 
+const productID = ref<number>()
+const productName = ref<string>()
+const productDescription = ref<string>()
+const productPrice = ref<string>()
+const productStock = ref<number>()
+const productCategory = ref<ProductCategory>(productCategories[0])
 </script>
 
 <template>
-	<div class="border border-2 border-secondary-subtle rounded-4 c">
-		<h1>Ajouter un composant</h1>
+	<div class="border border-2 border-secondary-subtle rounded-4 p-4">
+		<h1>{{ props.formTitle }}</h1>
+	
+		<form>
+			<div class="text-start my-2">
+				<label class="my-2" for="product-id">ID du produit</label>
+				<input v-model="productID" class="border-2 rounded-2 form-control" type="number" id="product-id" placeholder="0">
+			</div>
+
+			<div class="text-start my-2">
+				<label class="my-2" for="product-name">Nom du produit</label>
+				<input v-model="productName" class="border-2 rounded-2 form-control" type="text" id="product-name" placeholder="...">
+			</div>
+
+			<div class="text-start my-2">
+				<label class="my-2" for="product-description">Description</label>
+				<input v-model="productDescription" class="border-2 rounded-2 form-control" type="text" id="product-description" placeholder="...">
+			</div>
+
+			<div class="text-start my-2">
+				<label class="my-2" for="product-price">Prix</label>
+				<input v-model="productPrice" class="border-2 rounded-2 form-control" type="text" id="product-price" placeholder="0.00$">
+			</div>
+
+			<div class="text-start my-2">
+				<label class="my-2" for="product-stock">Inventaire</label>
+				<input v-model="productStock" class="border-2 rounded-2 form-control" type="number" id="product-stock" placeholder="0">
+			</div>
+
+			<div class="text-start my-2">
+				<label class="my-2" for="product-category">Catégorie</label>
+				<select v-model="productCategory" class="border-2 rounded-2 form-control" id="product-category">
+					<option v-for="category in productCategories" :value="category">
+						{{ category.name }}: {{ category.description }}
+					</option>
+				</select>
+			</div>
+		</form>
 	</div>
 </template>
 
