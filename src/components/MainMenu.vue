3c3e44e641b9ec6b744fe8f0d20c1740278d5ea2
xavier-lepage <2445275@csfoy.ca>
2026-03-01 17:01:46 -0500

    Implemented product list in main menu
    Added product list to main menu and seeding data for demonstration.

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -8,6 +8,7 @@ import Header from './Header.vue';
 import ProductForm from './ProductForm/ProductForm.vue';
 import ProductList from './ProductList/ProductList.vue';
 import type { Product } from '../models/Product';
+import { productCategories } from '../models/ProductCategory';
 
 /*	Documentation utilisée pour le collapse bootstrap:
 	https://vuejs.org/guide/essentials/template-refs
@@ -15,14 +16,41 @@ import type { Product } from '../models/Product';
 	https://getbootstrap.com/docs/5.3/components/collapse/#via-javascript
 */
 
+// Données de seed
+const productList = ref<Product[]>([
+	{
+		id: 1,
+		name: "f9 Extreme 420KF-X3D Pro-cessor @ 6.70 Ghz",
+		description: "Processeur grand-public le plus rapide.",
+		brand: "Fasty",
+		price: 420.99,
+		stock: 4,
+		productCategory: productCategories[0]
+	},
+	{
+		id: 2,
+		name: "Fast-9 ATX Motherboard with Wi-Fi",
+		description: "Carte mère ATX haute performance.",
+		brand: "susA",
+		price: 99.99,
+		stock: 6,
+		productCategory: productCategories[1]
+	}
+]);
 const currentAction = ref<ProductAction>(ProductAction.NONE);
+const isListShown = ref<boolean>(false);
 const formTitle = ref<string>('Ajouter un composant');
-const productForm = useTemplateRef("productForm");
+const productListElement = useTemplateRef("productList");
+const productFormElement = useTemplateRef("productForm");
+let productListCollapse: Collapse;
 let productFormCollapse: Collapse;
 
 onMounted(() => {
-	if (productForm.value !== null) {
-		productFormCollapse = new Collapse(productForm.value, { toggle: false });
+	if (productFormElement.value !== null) {
+		productFormCollapse = new Collapse(productFormElement.value, { toggle: false });
+	}
+	if (productListElement.value !== null) {
+		productListCollapse = new Collapse(productListElement.value, { toggle: false });
 	}
 });
 
@@ -30,6 +58,15 @@ function toggleAddProductForm(): void {
 	manageProductFormVisibility(ProductAction.ADD);
 }
 
+function toggleProductList(): void {
+	if (isListShown.value) {
+		productListCollapse.hide();
+	}
+	else {
+		productListCollapse.show();
+	}
+	isListShown.value = !isListShown.value;
+}
 
 function manageProductFormVisibility(actionPerformed: ProductAction): void {
 	if (currentAction.value === actionPerformed) {
@@ -43,8 +80,10 @@ function manageProductFormVisibility(actionPerformed: ProductAction): void {
 }
 
 function handleAddProduct(product: Product) {
-	console.log(product);
+	productList.value?.push(product);
 	manageProductFormVisibility(ProductAction.ADD);
+	productListCollapse.show();
+	isListShown.value = true;
 }
 </script>
 
@@ -54,10 +93,21 @@ function handleAddProduct(product: Product) {
 
 		<ActionButton
 			class="mx-2 my-4"
+			@click="toggleProductList()"
+			:label="'Liste des composants'" 
+			:priority="Priority.INFO">
+		</ActionButton>
+
+		<ActionButton
+			class="mx-2 my-4"
 			@click="toggleAddProductForm()" 
 			:label="'Ajouter un composant'" 
 			:priority="Priority.PRIMARY">
 		</ActionButton>
+
+		<div class="collapse" ref="productList">
+			<ProductList :products="productList"></ProductList>
+		</div>
 
 		<div class="collapse" ref="productForm">
 			<ProductForm @addProduct="handleAddProduct" :formTitle="formTitle"></ProductForm>
---
+++ src/components/ProductList/ProductList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Product } from '../../models/Product';
+import { ProductAction } from '../../models/ProductAction';
+import ProductDisplay from './ProductDisplay.vue';
+const props = defineProps<{
+	products: Product[]
+}>();
+</script>
+
+<template>
+	<div class="border border-2 border-secondary-subtle rounded-4 mb-4 p-4">
+		<h1>{{ ProductAction.LIST }}</h1>
+
+		<div class="mt-4" v-if="products.length === 0">Aucun produit dans la liste pour l'instant.</div>
+		<div class="mt-4" v-for="product in products">
+			<ProductDisplay :product="product"></ProductDisplay>
+		</div>
+	</div>
+</template>
